30e301c5d685d91f68e147978e57f57f401a7a65
michaeldcanady <michaeldcanady@yahoo.com>
2021-10-11 19:37:09 -0400

    Added requirements, license, readme, and contributing files

---
+++ README.md
@@ -0,0 +1,6 @@
+#AutoClicker
+
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/michaeldcanady/autoclicker/python-app?style=plastic)
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/michaeldcanady/autoclicker?style=plastic)
+![GitHub issues](https://img.shields.io/github/issues/michaeldcanady/autoclicker?style=plastic)
+![GitHub](https://img.shields.io/github/license/michaeldcanady/autoclicker?style=plastic)
--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Michael Canady
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import time
 import threading
 import sys
@@ -6,7 +21,6 @@ from PyQt5.QtGui import QIcon
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 from PyQt5.QtWidgets import QApplication, QComboBox, QHBoxLayout, QLabel, QLayout, QPushButton, QTextEdit, QWidget, QMainWindow, QGridLayout, QRadioButton, QLineEdit, QVBoxLayout, QFrame, QSpinBox
-from pynput.keyboard import Listener, KeyCode
 
 
 class Window(QWidget):
@@ -35,7 +49,7 @@ class Window(QWidget):
         self.initUI()
 
     @staticmethod
-    def _on_destroyed():
+    def _on_destroyed(self):
         print("destroyed")
         self.clickThread.exit()
         pass
